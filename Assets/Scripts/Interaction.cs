5db1c088121b052c30e77df2ca1804b58cc9b3a2
PeteGameDev <99825504+PeteGameDev@users.noreply.github.com>
2022-11-25 09:05:23 +0000

    changes

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
+    public GameObject purpleDoor;
+
+    void Start()
+    {
+        purpleDoor = GameObject.Find("Purple Door");
+    }
+
+
     void OnTriggerStay2D(Collider2D other){
-        if(other.CompareTag("Health Item") && Input.GetKeyDown(KeyCode.E))
+        if(other.CompareTag("Health Item"))
         {
             Debug.Log("Health added");
             gameObject.GetComponent<PlayerHealth>().playerHealth += 10f;
             Destroy(other.gameObject);
+        }
+
+        if(other.CompareTag("Door") && Input.GetKeyDown(KeyCode.E)){
+            Debug.Log("Opening Door");
+            Destroy(purpleDoor);
         }
     }
 }
